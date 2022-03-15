8edf9fa877a0ef00c1de3e430573a11eaf06ad61
fakir3007 <technic.repair@yandex.ru>
2022-03-15 16:57:05 +0500

    add script

---
+++ скрипт заполнения данными.sql
@@ -0,0 +1,65 @@
+insert into
+	perfomers(id, name)
+	
+values
+	(1, 'perfomers_1'),
+	(2, 'perfomers_2'),
+	(3, 'perfomers_3'),
+	(4, 'perfomers_4'),
+	(5, 'perfomers_5'),
+	(6, 'perfomers_6'),
+	(7, 'perfomers_7'),
+	(8, 'perfomers_8');
+insert into 
+	genre(id, name)
+	
+values 
+	(1, 'genre_1'),
+	(2, 'genre_2'),
+	(3, 'genre_3'),
+	(4, 'genre_4'),
+	(5, 'genre_5');
+insert into 
+	alboms (id, name, year)
+	
+values 
+	(1, 'alboms_1', 1990-12-15),
+	(2, 'alboms_2', 2000-11-23),
+	(3, 'alboms_3', 1997-07-25),
+	(4, 'alboms_4', 1945-05-09),
+	(5, 'alboms_5', 1768-11-25),
+	(6, 'alboms_6', 1989-09-09),
+	(7, 'alboms_7', 1999-10-31),
+	(8, 'alboms_8', 1991-07-30);
+insert into 
+	tracks(id, name, duration)
+
+values 
+	(1, 'tracks_1', 3.08),
+	(2, 'tracks_2', 2.30),
+	(3, 'tracks_3', 4.20),
+	(4, 'tracks_4', 2.40),
+	(5, 'tracks_5', 3.25),
+	(6, 'tracks_6', 2.55),
+	(7, 'tracks_7', 3.44),
+	(8, 'tracks_8', 5.25),
+	(9, 'tracks_9', 4.44),
+	(10, 'tracks_10', 2.22),
+	(11, 'tracks_11', 3.44),
+	(12, 'tracks_12', 5.55),
+	(13, 'tracks_13', 6.66),
+	(14, 'tracks_14', 1.1),
+	(15, 'tracks_15', 2.33);
+insert  into 
+	compilation (id, name, duration)
+	
+values 
+	(1, 'compilation_1', 2019-10-22),
+	(2, 'compilation_2', 2018-09-23),
+	(3, 'compilation_3', 2011-07-15),
+	(4, 'compilation_4', 2015-08-12),
+	(5, 'compilation_5', 2016-04-11),
+	(6, 'compilation_1', 1444-10-22),
+	(7, 'compilation_1', 1898-08-25),
+	(8, 'compilation_1', 1917-10-22);
+	
